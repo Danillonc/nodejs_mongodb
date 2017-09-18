031952ad7575087c9ce2ff781418874fa8336897
danillonc <danillonc@live.com>
2017-09-18 11:28:04 -0300

    manipulacao de urls requisitadas

--- noticias.js
+++ noticias.js
@@ -2,7 +2,17 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
 
-   res.end("<html><body>Portal de noticias</body></html>")
+   var categoria = req.url;
+   if(categoria == '/tecnologia'){
+      res.end("<html><body>Portal de tecnologia</body></html>");
+   }else if(categoria == '/moda'){
+      res.end("<html><body>Portal de moda</body></html>");
+   }else if (categoria == '/beleza'){
+      res.end("<html><body>Portal de beleza</body></html>");
+   }else{
+      res.end("<html><body>Portal de noticias</body></html>");
+   }
+   
 
 });
 
