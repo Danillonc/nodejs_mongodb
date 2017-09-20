679f6ca1c895bc040bbee9570a7bd5f5d2f46996
danillonc <danillonc@live.com>
2017-09-20 17:10:59 -0300

    add body-parser module and new routes

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,20 @@
-module.exports = function(app){
+module.exports = function(application){
   
-  app.get('/formulario_inclusao_noticia', function(req, res){
+  application.get('/formulario_inclusao_noticia', function(req, res){
     res.render("admin/form_add_noticia");
   });
 
-  app.post('/noticias/salvar', function(req, res){
-    res.send('chegou no servidor.');
+  application.post('/noticias/salvar', function(req, res){
+  	var noticias = req.body;
+    res.send(noticias);
+
+    var connection = application.config.dbConnection(); //getting the module dbConnection instantied on app variable.
+    var noticiasModel = application.app.models.noticiasModel; //getting module noticiasModel instantied on app variable.
+
+    noticiasModel.salvarNoticia(connection, function(error, result){
+       res.render("noticias/noticias", {noticias : result});
+    });
+
   });
 
 }
--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 var express = require('express'); // express module.
 var consign = require('consign'); // consign module.
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine','ejs'); //setting ejs to views
 app.set('views', './app/views'); //loading views files
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 /* initialize the consign module to autoloading routes and file dbConnection and inject into app instance*/
 consign()
