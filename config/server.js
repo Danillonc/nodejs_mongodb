b5867405590ad607404090f41f62232db4356049
danillonc <danillonc@live.com>
2017-09-22 14:37:19 -0300

    added express-validator module

--- config/server.js
+++ config/server.js
@@ -1,12 +1,14 @@
 var express = require('express'); // express module.
 var consign = require('consign'); // consign module.
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); //body-parser module.
+var expressValidator = require('express-validator'); //express-validator module.
 
 var app = express();
 app.set('view engine','ejs'); //setting ejs to views
 app.set('views', './app/views'); //loading views files
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 /* initialize the consign module to autoloading routes and file dbConnection and inject into app instance*/
 consign()
