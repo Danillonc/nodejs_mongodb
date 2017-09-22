adaa7e00aa6f5aad18a2ff8fe4db157775882432
danillonc <danillonc@live.com>
2017-09-22 17:25:18 -0300

    validation with express-validator on form add noticia

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,19 @@ module.exports = function(application){
 
   application.post('/noticias/salvar', function(req, res){
   	var noticia = req.body;
+    
+    req.assert('titulo','Titulo é obrigatório.').notEmpty(); 
+    req.assert('resumo', 'Resumo é obrigatório.').notEmpty();
+    req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+    req.assert('autor','Autor é obrigatório').notEmpty();
+    req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+
+    var erros = req.validationErrors();
+
+    if(erros){
+      res.render("admin/form_add_noticia");
+      return;
+    }
 
     var connection = application.config.dbConnection(); //getting the module dbConnection instantied on app variable.
     var noticiasModel = new application.app.models.noticiasDAO(connection); //getting module noticiasModel instantied on app variable.
